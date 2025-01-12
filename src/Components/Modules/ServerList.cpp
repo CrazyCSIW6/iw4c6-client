af90432fd27fcd4a85a2c2f43a350b62db320ad0
CrazyCSIW6 <ecksdeecrazytest@gmail.com>
2025-01-12 13:25:00 +1030

    use iw4.zip instead of alterware

--- src/Components/Modules/ServerList.cpp
+++ src/Components/Modules/ServerList.cpp
@@ -980,7 +980,7 @@ namespace Components
 			});
 
 		// Set default masterServerName + port and save it 
-		Utils::Hook::Set<const char*>(0x60AD92, "server.alterware.dev");
+		Utils::Hook::Set<const char*>(0x60AD92, "master.iw4.zip");
 		Utils::Hook::Set<std::uint8_t>(0x60AD90, Game::DVAR_NONE); // masterServerName
 		Utils::Hook::Set<std::uint8_t>(0x60ADC6, Game::DVAR_NONE); // masterPort
 
